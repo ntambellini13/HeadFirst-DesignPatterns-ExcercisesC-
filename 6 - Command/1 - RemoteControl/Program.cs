2f53c4d67791845a13857c5faff2f63b06d32c93
ntambellini13 <40220159+ntambellini13@users.noreply.github.com>
2020-07-14 08:28:36 -0600

    Chapter 6 - Command Pattern (Remote Control)

---
+++ 6 - Command/1 - RemoteControl/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RemoteControl.Model;
+using RemoteControl.Tests;
+
+// Command - Encapsulates a request as an object, thereby letting you parameterize clients with different
+//           requests, queue or log requests and support undo operations.
+
+namespace RemoteControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoteControlTest.Run();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 6 - Command/4 - RemoteControl - Macro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RemoteControlAPI.Model;
+using RemoteControlAPI.Tests;
+
+// Command - Encapsulates a request as an object, thereby letting you parameterize clients with different
+//           requests, queue or log requests and support undo operations.
+
+namespace RemoteControlAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoteLoader.Run();
+            Console.WriteLine();
+            RemoteWithMacro.Run();
+            Console.ReadKey();
+        }
+    }
+}
