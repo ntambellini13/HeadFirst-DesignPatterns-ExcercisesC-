40c9f36c3d115c0a4fb572c6aa46976516a75c4d
ntambellini13 <40220159+ntambellini13@users.noreply.github.com>
2020-07-15 09:00:38 -0600

    Chapter 7 - Adapter and Facade (TurkeyDuck and HomeTheatre)

---
+++ 7 - Adapter & Facade/1 - TurkeyDucks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TurkeyDucks.Model;
+using TurkeyDucks.Tests;
+
+// Adapter- Converts the interface of a class into another interface the client expects. Adapters let
+//          classes work together that couldn't otherwise because of incompatible interfaces.
+
+namespace TurkeyDucks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DuckTestDrive.Run();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 7 - Adapter & Facade/2 - HomeTheater/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HomeTheater.Model;
+using HomeTheater.Tests;
+
+// Facade - provides a unified interface to a set of interface in a subsytem. Facade defines
+//          a higher-level interface that makes the subsystem easier to use.
+
+namespace HomeTheater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeTheaterTestDrive.Run();
+            Console.ReadKey();
+        }
+    }
+}
