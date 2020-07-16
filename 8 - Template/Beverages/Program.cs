82b42a604d59c25c604bf87d08fd5c7209ef2fef
ntambellini13 <40220159+ntambellini13@users.noreply.github.com>
2020-07-16 07:41:08 -0600

    Chapter 8 Template Method Pattern (Beverages)

---
+++ 8 - Template/Beverages/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Beverages.Model;
+using Beverages.Tests;
+
+// Template Method - Defines the skeleton of an algorithm in a method, deferring some steps to subclasses.
+//                   Template Method lets subclasses redefine certain steps of an algorithm without changing
+//                   the algorithms structure.
+
+namespace Beverages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BeverageTestDrive.Run();
+            Console.ReadKey();
+        }
+    }
+}
