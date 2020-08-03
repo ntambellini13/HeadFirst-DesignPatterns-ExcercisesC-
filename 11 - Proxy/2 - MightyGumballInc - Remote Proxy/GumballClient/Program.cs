6005a320180e5124f2f5228d3d8e536fcbf3c77e
Nico Tambellini <ntambellini13@gmail.com>
2020-08-02 21:50:24 -0600

    Chapter 11 - Proxy (Remote, Virtual and Dynamic Protection)

---
+++ 11 - Proxy/2 - MightyGumballInc - Remote Proxy/GumballClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GumballClient.Model;
+using GumballClient.Tests;
+
+//Proxy - Provides a surrogate or placeholder for another object to control access to it.
+
+namespace GumballClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GumballMonitorTestDrive.Run(args);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 11 - Proxy/4 - MatchMaker- Dynamic Protection Proxy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MatchMaker.Tests;
+
+//Proxy - Provides a surrogate or placeholder for another object to control access to it.
+
+namespace MatchMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatchMakingTestDrive.Run();
+            Console.ReadKey();
+        }
+    }
+}
